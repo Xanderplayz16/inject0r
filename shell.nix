5a1f8be3086d6eda888d2928233c62c3e696ab31
Xander Mckay <xanderm@noyoudon.t>
2024-09-21 14:26:51 -0400

    Remove some unnessecary stuff from shell.nix

--- shell.nix
+++ shell.nix
@@ -3,8 +3,6 @@ let
 in
   pkgs.mkShell {
     nativeBuildInputs = with pkgs; [
-      stdenv.cc.cc.lib # make bcrypt js happy
       bun
     ];
-    LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${pkgs.stdenv.cc.cc.lib}/lib"; # makes bcrypt js happy
   }
